6dbb757d808470a08639d0a627fc2a1a45e439a2
Asaduzzaman_mac <samrat.asad@gmail.com>
2022-06-10 10:04:27 +0600

    google sign in setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,14 +23,75 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Future<FirebaseApp> _firebaseApp;
+  bool isLoggedIn = false;
+  late var name;
+
+  @override
+  void initState() {
+    super.initState();
+    _firebaseApp = Firebase.initializeApp();
+  }
+
+  void _signOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
+
+  void googleSignIn() async {
+    final googleSignIn = GoogleSignIn();
+    final signInAccount = await googleSignIn.signIn();
+    final googleAccountAuthentication = await signInAccount?.authentication;
+    final credential = GoogleAuthProvider.credential(
+        accessToken: googleAccountAuthentication?.accessToken,
+        idToken: googleAccountAuthentication?.idToken);
+    await FirebaseAuth.instance.signInWithCredential(credential);
+
+    if (FirebaseAuth.instance.currentUser != null) {
+      print('Google Authentication successful ');
+      print('${FirebaseAuth.instance.currentUser?.displayName} signed in');
+      setState(() {
+        isLoggedIn = true;
+        name = FirebaseAuth.instance.currentUser?.displayName;
+      });
+    } else {
+      print('Unable to sign in');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Firebase Tuto"),
+      body: FutureBuilder(
+        future: _firebaseApp,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting)
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                isLoggedIn ? Text("$name signed in") : Text(''),
+                ElevatedButton(
+                  onPressed: isLoggedIn ? _signOut : googleSignIn,
+                  child: isLoggedIn
+                      ? Text('Sign Out')
+                      : Text('sign in with '
+                          'google'),
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
